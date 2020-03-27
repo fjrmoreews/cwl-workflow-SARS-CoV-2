a8f42af816affc1fb5ee0285a826e05f3d51a9cd
Miguel Boland <migueldboland@gmail.com>
2020-03-27 19:58:54 +0000

    Added fetch_static_files.sh to fetch some static files

---
+++ PreProcessing/fetch_static_files.sh
@@ -0,0 +1,6 @@
+mkdir ./static;
+wget -c -O ./static/hg38.fna.gz ftp://ftp.ncbi.nlm.nih.gov/refseq/H_sapiens/annotation/GRCh38_latest/refseq_identifiers/GRCh38_latest_genomic.fna.gz;
+cd ./static && gunzip hg38.fna.gz;
+
+bwa index hg38.fna
+samtools faidx hg38.fna
